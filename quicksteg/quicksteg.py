486c476f6c0f9954c548b4f50c3ef3771273e6c5
S-JM <33537725+S-JM@users.noreply.github.com>
2024-12-13 15:08:10 -0500

    added dependency check

--- quicksteg/quicksteg.py
+++ quicksteg/quicksteg.py
@@ -1,27 +1,26 @@
 import os
 import sys
 
-## Check dependencies
-#dependencies = ['steghide', 'foremost', 'exiftool', 'strings', 'binwalk']
-#for i in dependencies:
-#    if not os.system('which ' + i):
-#        print(i + ' is installed')
-#    else:
-#        print('Installing' + i)
-#        os.system('sudo apt-get install ' + i)
-
 def main():
     try:
+        ## Check dependencies
+        dependencies = ['steghide', 'foremost', 'exiftool', 'strings', 'binwalk']
+        for i in dependencies:
+            if not os.system('which ' + i):
+                print(i + ' is installed')
+            else:
+                print('You are missing ' + i + '. Please install it using your package manager.')
+
         file = input("Enter the location of the file: ")
         if os.path.exists(file):
-            os.system('steghide info ' + file + ' > steghide.txt')
-            os.system('foremost ' + file + ' > foremost.txt')
-            os.system('exiftool ' + file + ' > exiftool.txt')
-            os.system('strings -a ' + file + ' > strings.txt')
-            os.system('binwalk -e ' + file + ' > binwalk.txt')
+            os.system('steghide info ' + file + ' >> quicksteg.out')
+            os.system('foremost ' + file + ' >> quicksteg.out')
+            os.system('exiftool ' + file + ' >> quicksteg.out')
+            os.system('strings -a ' + file + ' >> quicksteg.out')
+            os.system('binwalk -e ' + file + ' >> quicksteg.out')
         else:
             print('The file does not exist')
     except KeyboardInterrupt:
-        print('Exiting...')
+        print(' Exiting...')
         sys.exit(0)
 main()
