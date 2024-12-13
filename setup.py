502546e23982fe53b9416b2abf9c0176f2081f16
S-JM <33537725+S-JM@users.noreply.github.com>
2024-12-13 14:22:14 -0500

    version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='quicksteg',
-      version='0.0.1',
+      version='0.0.2',
       description='Quick steganography tools',
       license='GPLv3',
       classifiers=[
