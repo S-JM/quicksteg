b18076eeaf671ebae79af6c26a9c06630b96366f
S-JM <33537725+S-JM@users.noreply.github.com>
2024-12-13 13:45:05 -0500

    Added setup.py

--- quicksteg.py
+++ quicksteg.py
@@ -1,19 +1,21 @@
 import os
 
-# Check dependencies
-dependencies = ['steghide', 'foremost', 'exiftool', 'strings', 'binwalk']
-for i in dependencies:
-    if not os.system('which ' + i):
-        print(i + ' is installed')
-    else:
-        print('Installing' + i)
-        os.system('sudo apt-get install ' + i)
+## Check dependencies
+#dependencies = ['steghide', 'foremost', 'exiftool', 'strings', 'binwalk']
+#for i in dependencies:
+#    if not os.system('which ' + i):
+#        print(i + ' is installed')
+#    else:
+#        print('Installing' + i)
+#        os.system('sudo apt-get install ' + i)
 
+def main():
+    file = input("Enter the location of the file: ")
 
-file = input("Enter the location of the file: ")
+    os.system('steghide info ' + file + ' > steghide.txt')
+    os.system('foremost ' + file + ' > foremost.txt')
+    os.system('exiftool ' + file + ' > exiftool.txt')
+    os.system('strings -a ' + file + ' > strings.txt')
+    os.system('binwalk -e ' + file + ' > binwalk.txt')
 
-os.system('steghide info ' + file + ' > steghide.txt')
-os.system('foremost ' + file + ' > foremost.txt')
-os.system('exiftool ' + file + ' > exiftool.txt')
-os.system('strings -a ' + file + ' > strings.txt')
-os.system('binwalk -e ' + file + ' > binwalk.txt')
+main()
